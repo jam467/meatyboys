e2aba4e2364bd75121e2ddd0430d01557cbb0922
jam467 <jdunlop467@gmail.com>
2018-03-04 11:34:36 +1100

    add require

--- downloads/saveSquads.js
+++ downloads/saveSquads.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 request({
 	gzip: true,
--- routes/getSquads.js
+++ routes/getSquads.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
