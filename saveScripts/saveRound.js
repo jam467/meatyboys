77854dfb506b12bd2b460b9a6a8b3153597c6af4
jam467 <jdunlop467@gmail.com>
2022-03-11 20:21:02 +1100

    round error

--- saveScripts/saveRound.js
+++ saveScripts/saveRound.js
@@ -2,31 +2,29 @@ var request = require('request');
 var fs = require('fs');
 var loc = require('./saveLocation');
 
-fs.readFile(loc+'downloads/currentseason.json', function(err,data){
+fs.readFile(loc + 'downloads/currentseason.json', function (err, data) {
 	var data = JSON.parse(data);
 	var round = 0;
 	var ofRound = {};
 	var date = new Date();
 	// console.log(date);
-	var isoDateTime = new Date(date.getTime() - (-660 * 60000)).toISOString();
+	var isoDateTime = new Date();
 	console.log(isoDateTime);
-	for(var i =0;i<data.length;i++){
+	for (var i = 0; i < data.length; i++) {
 		var gameDate = (new Date(data[i].datetime));
-		if(gameDate<new Date(isoDateTime)){
-			var twoDays = new Date(date.getTime() - (-660 * 60000)+(60*60*24*2));
-			if(twoDays<gameDate){
-				ofRound = data[i].round;
-			}else{
-				ofRound = data[i].round+1;
-			}
-			
+		var lesstwoDays = new Date(gameDate.getTime() - (1000 * 60 * 60 * 24 * 3));
+		console.log(lesstwoDays, date)
+		if (lesstwoDays > date) {
+			ofRound = data[i-1].round;
+			i=data.length;
 		}
-		console.log(ofRound)
-	}
-		fs.writeFile(loc+'downloads/round.json', JSON.stringify({"round":ofRound,"official":ofRound}), function (err) {
-  						//if (err) throw err;
- 						console.log('Saved!');
-					});
 
 	}
+	console.log(ofRound)
+	fs.writeFile(loc + 'downloads/round.json', JSON.stringify({ "round": ofRound, "official": ofRound }), function (err) {
+		//if (err) throw err;
+		console.log('Saved!');
+	});
+
+}
 );
